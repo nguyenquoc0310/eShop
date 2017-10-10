d12bb4c3f444b184e39dc34a0242368abc1b7c32
nguyenquoc0310 <nguyenquoc0310.github.com>
2017-10-10 22:18:33 +0700

    Validate comment

--- src/app/comment/comment.component.ts
+++ src/app/comment/comment.component.ts
@@ -4,6 +4,7 @@ import {CommentService} from '../comment-service';
 import {Product} from '../product';
 import {ProductService} from '../product.service';
 import {isUndefined} from 'util';
+import {isEmpty} from 'rxjs/operator/isEmpty';
 
 @Component({
   selector: 'app-comment',
@@ -14,6 +15,7 @@ export class CommentComponent implements OnInit {
   @Input() product: Product;
   comments: Array<Comment> = new Array<Comment>();
   comment: Comment = new Comment();
+  isError: Boolean = false;
 
   constructor(private commentService: CommentService,
               private productService: ProductService) {
@@ -23,16 +25,28 @@ export class CommentComponent implements OnInit {
     this.getListComment();
   }
 
+  checkInput() {
+    if (this.comment.name && this.comment.comment) {
+      this.isError = false;
+    } else {
+      this.isError = true;
+    }
+  }
+
   createComment() {
-    this.comment.productId = this.product._id;
-    this.comment.date = Date.now();
-    this.commentService.createComment(this.comment)
-      .then(comment => {
-        this.increaseNumComment();
-        this.getListComment();
-        this.comment = new Comment();
-      })
-      .catch(err => console.log(err));
+    this.checkInput();
+
+    if (this.isError === false) {
+      this.comment.productId = this.product._id;
+      this.comment.date = Date.now();
+      this.commentService.createComment(this.comment)
+        .then(comment => {
+          this.increaseNumComment();
+          this.getListComment();
+          this.comment = new Comment();
+        })
+        .catch(err => console.log(err));
+    }
   }
 
   getListComment() {
